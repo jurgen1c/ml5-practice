a713b778eea3d887d4a8b316608fa3039d2f2fe1
jurgen1c <jurgen1c@gmail.com>
2020-12-17 16:06:19 -0600

    Draw tracking of positions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let video;
 let poseNet;
 let pose;
+let skeleton;
 
 function setup(){
   createCanvas(640, 480);
@@ -13,7 +14,8 @@ function setup(){
 function gotPoses(poses){
   console.log(poses);
   if(poses.length > 0){
-    pose = poses[0].pose
+    pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
   }
 }
 
@@ -22,10 +24,35 @@ function modelLoaded(){
 }
 
 function draw(){
-  image(video, 0 ,0);
+  translate(video.width, 0);
+  scale(-1, 1);
+  image(video, 0 ,0, video.width, video.height);
 
   if(pose){
+
+    let eyeR = pose.rightEye;
+    let eyeL = pose.leftEye;
+    let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y); 
+
     fill(255, 0, 0);
-    ellipse(pose.nose.x, pose.nose.y, 64);
+    ellipse(pose.nose.x, pose.nose.y, d);
+    fill(0, 0, 255);
+    ellipse(pose.rightWrist.x, pose.rightWrist.y, 32);
+    ellipse(pose.leftWrist.x, pose.leftWrist.y, 32);
+
+    for(let i = 0; i < pose.keypoints.length; i++){
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill(0,255,0);
+      ellipse(x, y, 16, 16);
+    }
+
+    for(let i = 0; i < skeleton.length; i++){
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
+      strokeWeight(2);
+      stroke(255);
+      line(a.position.x, a.position.y, b.position.x, b.position.y);
+    }
   }
 }
